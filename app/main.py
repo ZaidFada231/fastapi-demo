bc6b022bc7c651e78bec1a488db481779ebc308c
Zaid <zaid.fada@gmail.com>
2024-03-30 18:36:24 -0400

    added new static files

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+import os
+import MySQLdb
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
@@ -9,6 +11,12 @@ import boto3
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+DBHOST = os.environ.get("DBHOST")
+DBUSER = os.environ.get("DBUSER")
+DBPASS = os.environ.get("DBPASS")
+DB = "mst3k"  # replace with your UVA computing ID / database name
 # The URL for this API has a /docs endpoint that lets you see and test
 # your various endpoints/methods.
 
